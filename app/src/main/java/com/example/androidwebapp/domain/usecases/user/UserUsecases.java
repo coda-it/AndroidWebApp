edfa2b47e530fb5457eba181fd9106e5f392d174
Oskar Szura <oskar.szura@gmail.com>
2021-10-11 09:25:22 +0200

    CODA-226 - Login page (#1)

---
+++ app/src/main/java/com/example/androidwebapp/domain/usecases/user/UserUsecases.java
@@ -0,0 +1,18 @@
+package com.example.androidwebapp.domain.usecases.user;
+
+import android.app.Activity;
+import android.util.Log;
+
+import com.example.androidwebapp.data.repositories.user.UserRepository;
+
+public class UserUsecases {
+    UserRepository userRepository;
+
+    public UserUsecases(Activity activity) {
+        this.userRepository = new UserRepository(activity);
+    }
+
+    public void login(String login, String password) {
+        this.userRepository.login(login, password);
+    }
+}
